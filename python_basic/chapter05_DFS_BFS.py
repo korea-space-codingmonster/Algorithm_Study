c175a15886efc5c154ca8d5bb96eff664639bead
repl.it user <replituser@example.com>
2021-01-21 08:58:11 +0000

    add

--- python_basic/chapter05_DFS_BFS.py
+++ python_basic/chapter05_DFS_BFS.py
@@ -275,57 +275,102 @@
 # 3. 출력
 
 # 최종 값 출력
-from collection import deque
+# from collection import deque
 
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
-graph = []
-graph.append(list(map(int, input())))
+# graph = []
+# graph.append(list(map(int, input())))
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
 
-def bfs(x, y):
-  queue = deque()
-  queue.append((x, y))
+# def bfs(x, y):
+#   queue = deque()
+#   queue.append((x, y))
 
-  while queue:
-    x, y = deque.popleft()
+#   while queue:
+#     x, y = deque.popleft()
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-      if nx < 0 or ny < 0 or nx >= n or ny >= m:
-        return False
-      if graph[nx][ny] == 0:
-        continue
-      if graph[nx][ny] == 1:
-        graph[nx][ny] = graph[x][y] + 1
-        queue.append((nx, ny))
-  return graph[n - 1][m - 1]
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+#       if nx < 0 or ny < 0 or nx >= n or ny >= m:
+#         return False
+#       if graph[nx][ny] == 0:
+#         continue
+#       if graph[nx][ny] == 1:
+#         graph[nx][ny] = graph[x][y] + 1
+#         queue.append((nx, ny))
+#   return graph[n - 1][m - 1]
         
-print(bfs(0, 0))
+# print(bfs(0, 0))
 
 
 
 
 #-------------------------------------------------------------------------
-문제
-그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+# 문제
+# 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
 
-입력
-첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
+# 입력
+# 첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
 
-출력
-첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
+# 출력
+# 첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
 
-예제 입력 1  복사
-4 5 1
-1 2
-1 3
-1 4
-2 4
-3 4
-예제 출력 1  복사
-1 2 4 3
-1 2 3 4
+# 예제 입력 1  복사
+# 4 5 1
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
+# 예제 출력 1  복사
+# 1 2 4 3
+# 1 2 3 4
+
+from collections import deque
+import sys
+#n = 노드개수, m = 간선개수 start = 시작노 
+n, m, start = map(int, sys.stdin.readline().split())
+
+edgelist = [list([]) for _ in range(n + 1)]
+
+for _ in range(m):
+  n1, n2 = map(int, sys.stdin.readline().split())
+  edgelist[n1].append(n2)
+  edgelist[n2].append(n1)
+
+for edge in edgelist:
+  edge.sort()
+
+def bfs(edgelist, start, visited):
+  queue = deque([start])
+  visited[start] = True
+  while queue:
+    p = queue.popleft()
+    print (p, end = ' ')
+    for i in edgelist[p]:
+        if not visited[i]:
+          queue.append(i)#1과 연관된 모든 노드를 추가
+          visited[i] = True#방문처리해버리기
+
+
+def dfs(edgelist, start, visited):
+  visited[start] = True
+  print(start, end = ' ')
+  #현재 노드와 연결된 다른 노드를 재귀적으로 방문
+  for i in edgelist[start]:
+    if not visited[i]:
+      dfs(edgelist, i, visited)
+
+visited = [False] * m
+dfs(edgelist, start, visited)
+print()
+visited = [False] * m
+bfs(edgelist, start, visited)
+
+
+
+#----------------------------------------------------------------------------
