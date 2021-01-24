a4c3adbe9de555fef2c0cd115f27caf166e8cfd2
repl.it user <replituser@example.com>
2021-01-24 07:18:58 +0000

    add

--- python_basic/chapter05_DFS_BFS.py
+++ python_basic/chapter05_DFS_BFS.py
@@ -330,47 +330,317 @@
 # 1 2 4 3
 # 1 2 3 4
 
+# from collections import deque
+# import sys
+# #n = 노드개수, m = 간선개수 start = 시작노드 
+# n, m, start = map(int, sys.stdin.readline().split())
+
+# edgelist = [list([]) for _ in range(n + 1)]
+
+# for _ in range(m):
+#   n1, n2 = map(int, sys.stdin.readline().split())
+#   edgelist[n1].append(n2)
+#   edgelist[n2].append(n1)
+
+# for edge in edgelist:
+#   edge.sort()
+
+# def bfs(edgelist, start, visited):
+#   queue = deque([start])
+#   visited[start] = False
+#   while queue:
+#     p = queue.popleft()
+#     print (p, end = ' ')
+#     for i in edgelist[p]:
+#         if visited[i]:
+#           queue.append(i)#1과 연관된 모든 노드를 추가
+#           visited[i] = False#방문처리해버리기
+
+
+# def dfs(edgelist, start, visited):
+#   visited[start] = True
+#   print(start, end = ' ')
+#   #현재 노드와 연결된 다른 노드를 재귀적으로 방문
+#   for i in edgelist[start]:
+#     if not visited[i]:
+#       dfs(edgelist, i, visited)
+
+# visited = [False] * m
+# dfs(edgelist, start, visited)
+# print()
+# bfs(edgelist, start, visited)
+
+
+
+#---------------------------------------------------------------------------
+#BFS 문제(너비 우선탐색)
+# 문제
+# N×M크기의 배열로 표현되는 미로가 있다.
+
+# 1	0	1	1	1	1
+# 1	0	1	0	1	0
+# 1	0	1	0	1	1
+# 1	1	1	0	1	1
+# 미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
+# 위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+
+# 입력
+# 첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+
+# 출력
+# 첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+
+# 예제 입력 1  복사
+# 4 6
+# 101111
+# 101010
+# 101011
+# 111011
+# 예제 출력 1  복사
+# 15
+
+# from collections import deque
+
+# dx = [0, 0 ,1, -1]
+# dy = [1, -1, 0, 0]
+
+# n, m = map(int, input().split())
+# a = [list(map(int, list(input()))) for _ in range(n)]
+# q = deque()
+# check = [[False] * m for _ in range(n)]
+# dist = [[0] * m for _ in range(n)]
+
+# q.append((0, 0))
+# check[0][0] = True
+# dist[0][0] = 1
+
+# while q:
+#   x, y = q.popleft()
+#   for k in range(4):
+#     nx, ny = x + dx[k], y + dy[k]
+#     if 0 <= nx < n and 0 <= ny < m:
+#       if check[nx][ny] == False and a[nx][ny] == 1:
+#         q.append((nx, ny))
+#         dist[nx][ny] = dist[x][y] + 1
+#         checkp[nx][ny] = True
+
+# print(dist[n - 1][m - 1])
+
+
+
+
+
+#-----------------------------------------------------------------
+# 문제
+# <그림 1>과 같이 정사각형 모양의 지도가 있다. 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다. 여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다. 대각선상에 집이 있는 경우는 연결된 것이 아니다. <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
+
+
+
+# 입력
+# 첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고, 그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
+
+# 출력
+# 첫 번째 줄에는 총 단지수를 출력하시오. 그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
+
+# 예제 입력 1  복사
+# 7
+# 0110100
+# 0110101
+# 1110101
+# 0000111
+# 0100000
+# 0111110
+# 0111000
+# 예제 출력 1  복사
+# 3
+# 7
+# 8
+# 9
+
+#단지 수, 단지별 아파수 오름차순 출력
+# import sys
+
+# n = int(input())
+
+# def dfs(graph, count ,x, y):
+#   graph[x][y] == 0
+
+#   dx = [-1, 1, 0, 0]
+#   dy = [0, 0, 1, -1]
+
+#   for i in range(4):
+#     nx = x + dx[i]
+#     ny = y + dy[i]
+#     if 0 <= nx < n and 0 <= ny < n:
+#       if graph[nx][ny] == 1:
+#         count = dfs(graph, count+1, nx, ny) 
+#   return count
+
+
+# graph = [list(map(int, list(input()))) for _ in range(n)]
+
+# ans = []
+# for i in range(n):
+#   for j in range(n):
+#     if graph[i][j] == 1:
+#       ans.append(dfs(graph, 1, i, j))
+
+# print(len(ans))
+# for i in sorted(ans):
+#   print(i)
+
+
+
+
+#----------------------------------------------------------------
+# 문제
+# 철수의 토마토 농장에서는 토마토를 보관하는 큰 창고를 가지고 있다. 토마토는 아래의 그림과 같이 격자 모양 상자의 칸에 하나씩 넣어서 창고에 보관한다. 
+
+
+
+# 창고에 보관되는 토마토들 중에는 잘 익은 것도 있지만, 아직 익지 않은 토마토들도 있을 수 있다. 보관 후 하루가 지나면, 익은 토마토들의 인접한 곳에 있는 익지 않은 토마토들은 익은 토마토의 영향을 받아 익게 된다. 하나의 토마토의 인접한 곳은 왼쪽, 오른쪽, 앞, 뒤 네 방향에 있는 토마토를 의미한다. 대각선 방향에 있는 토마토들에게는 영향을 주지 못하며, 토마토가 혼자 저절로 익는 경우는 없다고 가정한다. 철수는 창고에 보관된 토마토들이 며칠이 지나면 다 익게 되는지, 그 최소 일수를 알고 싶어 한다.
+
+# 토마토를 창고에 보관하는 격자모양의 상자들의 크기와 익은 토마토들과 익지 않은 토마토들의 정보가 주어졌을 때, 며칠이 지나면 토마토들이 모두 익는지, 그 최소 일수를 구하는 프로그램을 작성하라. 단, 상자의 일부 칸에는 토마토가 들어있지 않을 수도 있다.
+
+# 입력
+# 첫 줄에는 상자의 크기를 나타내는 두 정수 M,N이 주어진다. M은 상자의 가로 칸의 수, N은 상자의 세로 칸의 수를 나타낸다. 단, 2 ≤ M,N ≤ 1,000 이다. 둘째 줄부터는 하나의 상자에 저장된 토마토들의 정보가 주어진다. 즉, 둘째 줄부터 N개의 줄에는 상자에 담긴 토마토의 정보가 주어진다. 하나의 줄에는 상자 가로줄에 들어있는 토마토의 상태가 M개의 정수로 주어진다. 정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸을 나타낸다.
+
+# 토마토가 하나 이상 있는 경우만 입력으로 주어진다.
+
+# 출력
+# 여러분은 토마토가 모두 익을 때까지의 최소 날짜를 출력해야 한다. 만약, 저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고, 토마토가 모두 익지는 못하는 상황이면 -1을 출력해야 한다.
+
+# 예제 입력 1  복사
+# 6 4
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 1
+# 예제 출력 1  복사
+# 8
+
+# 예제 입력 2  복사
+# 6 4
+# 0 -1 0 0 0 0
+# -1 0 0 0 0 0
+# 0 0 0 0 0 0
+# 0 0 0 0 0 1
+# 예제 출력 2  복사
+# -1
+# 예제 입력 3  복사
+# 6 4
+# 1 -1 0 0 0 0
+# 0 -1 0 0 0 0
+# 0 0 0 0 -1 0
+# 0 0 0 0 -1 1
+# 예제 출력 3  복사
+# 6
+# 예제 입력 4  복사
+# 5 5
+# -1 1 0 0 0
+# 0 -1 -1 -1 0
+# 0 -1 -1 -1 0
+# 0 -1 -1 -1 0
+# 0 0 0 0 0
+# 예제 출력 4  복사
+# 14
+
 from collections import deque
-import sys
-#n = 노드개수, m = 간선개수 start = 시작노 
-n, m, start = map(int, sys.stdin.readline().split())
 
-edgelist = [list([]) for _ in range(n + 1)]
+M, N = map(int, input().split())
+tots = [list(map(int, input().split())) for _ in range(N)]
+queue = deque() # 꼭 deque를 쓰지 않아도 됨
 
-for _ in range(m):
-  n1, n2 = map(int, sys.stdin.readline().split())
-  edgelist[n1].append(n2)
-  edgelist[n2].append(n1)
+for i in range(N):
+    for j in range(M):
+        if tots[i][j] == 1:
+            queue.append([i, j])
+            
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-for edge in edgelist:
-  edge.sort()
+# 여기부터 bfs
+while queue:
+    row, col = queue.popleft()
+    
+    for k in range(4):
+        _row = row + dy[k]
+        _col = col + dx[k]
+        
+        # 안익은게 있다면, 익은 것으로 바꿔준다.
+        if 0 <= _row < N and 0 <= _col < M and tots[_row][_col] == 0:
+            tots[_row][_col] = tots[row][col] + 1
+            queue.append([_row, _col])
+# bfs 끝------
 
-def bfs(edgelist, start, visited):
-  queue = deque([start])
-  visited[start] = True
+# -1이 존재해서 -2 값으로 비교
+result = -2
+# 안익은게 있는지 체크
+check_tot = False#0
+
+for i in tots:
+    for j in i:
+        if(j == 0):
+            check_tot = True#1
+        result = max(result, j)#제일 큰 값을 반환
+        
+if check_tot:#아직도 0이면 
+    print(-1)#-1 출력
+elif result == -1:
+    print(0)
+else:
+    print(result - 1)
+
+
+#---------------------------------------------------------------------------
+# 문제
+# 신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다. 한 컴퓨터가 웜 바이러스에 걸리면 그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
+
+# 예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자. 1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다. 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
+
+
+
+# 어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다. 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에는 컴퓨터의 수가 주어진다. 컴퓨터의 수는 100 이하이고 각 컴퓨터에는 1번 부터 차례대로 번호가 매겨진다. 둘째 줄에는 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수가 주어진다. 이어서 그 수만큼 한 줄에 한 쌍씩 네트워크 상에서 직접 연결되어 있는 컴퓨터의 번호 쌍이 주어진다.
+
+# 출력
+# 1번 컴퓨터가 웜 바이러스에 걸렸을 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 첫째 줄에 출력한다.
+
+# 예제 입력 1  복사
+# 7
+# 6
+# 1 2
+# 2 3
+# 1 5
+# 5 2
+# 5 6
+# 4 7
+# 예제 출력 1  복사
+# 4
+
+from sys import stdin
+read = stdin.readline
+dic = {}
+
+for i in range(int(read())):
+  dic[i + 1] = set()
+for j in range(int(read())):
+  a, b = map(int, read().split())
+  dic[a].add(b)
+  dic[b].add(a)
+
+def bfs(start, dic):
+  queue = [start]
   while queue:
-    p = queue.popleft()
-    print (p, end = ' ')
-    for i in edgelist[p]:
-        if not visited[i]:
-          queue.append(i)#1과 연관된 모든 노드를 추가
-          visited[i] = True#방문처리해버리기
+    for i in dic[queue.pop()]:
+      if i not in visited:
+        visited.append(i)
+        queue.append(i)
 
+visited = []
+bfs(1, dic)
+print(len(visited) - 1)
 
-def dfs(edgelist, start, visited):
-  visited[start] = True
-  print(start, end = ' ')
-  #현재 노드와 연결된 다른 노드를 재귀적으로 방문
-  for i in edgelist[start]:
-    if not visited[i]:
-      dfs(edgelist, i, visited)
-
-visited = [False] * m
-dfs(edgelist, start, visited)
-print()
-visited = [False] * m
-bfs(edgelist, start, visited)
-
-
-
-#----------------------------------------------------------------------------
