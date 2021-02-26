2c27de9361a5be035a6bdc04dd1177c1a39c9a55
repl.it user <replituser@example.com>
2021-02-26 05:07:42 +0000

    push

--- chapter05 DFS/음료수 얼려먹기.py
+++ chapter05 DFS/음료수 얼려먹기.py
@@ -31,3 +31,30 @@
 
 n, m = map(int, input().split())
 
+graph = []
+for i in range(n):
+  graph.append(list(map(int, input())))
+#graph = [list(map(int, input().split())) for _ in range(n)]
+
+def dfs(x, y):
+  if x <= -1 or x >= n or y <= -1 or y >= m:
+    return false
+  
+  if graph[x][y] == 0:
+    graph[x][y] = 1
+
+    dfs(x - 1, y)
+    dfs(x, y - 1)
+    dfs(x + 1, y)
+    dfs(x, y + 1)
+    return True
+  return  False
+
+
+result = 0
+for i in range(n):
+  for j in range(m):
+    if dfs(n, m) == True:
+      result += 1
+
+print(result)
